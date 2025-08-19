4021e0993ad7079995c2032c2d68c794d20a7166
Guilherme Costa <guilecosta01@gmail.com>
2025-08-19 18:02:14 -0300

    Corrigir URL do servidor no Swagger UI - Configurar Azure como servidor padrão em vez de localhost

---
+++ src/main/java/com/decolatech/easytravel/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.decolatech.easytravel.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server azureServer = new Server();
+        azureServer.setUrl("https://back-et.azurewebsites.net");
+        azureServer.setDescription("Azure Production Server");
+
+        Server localServer = new Server();
+        localServer.setUrl("http://localhost:8080");
+        localServer.setDescription("Local Development Server");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("EasyTravel API")
+                        .description("API para sistema de viagens EasyTravel")
+                        .version("1.0.0"))
+                .servers(List.of(azureServer, localServer));
+    }
+}
