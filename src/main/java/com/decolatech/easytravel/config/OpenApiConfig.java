92c8a96d4ea56761ca47f23d44039e2087e83004
Guilherme Costa <guilecosta01@gmail.com>
2025-08-20 11:04:10 -0300

    CORREÇÃO DEFINITIVA: Resolver conflito de beans OpenAPI que impedia inicialização - Remover SwaggerConfig duplicado

--- src/main/java/com/decolatech/easytravel/config/OpenApiConfig.java
+++ src/main/java/com/decolatech/easytravel/config/OpenApiConfig.java
@@ -2,10 +2,11 @@ package com.decolatech.easytravel.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import java.util.List;
 
@@ -13,23 +14,11 @@ import java.util.List;
 public class OpenApiConfig {
 
     @Bean
-    @Profile("prod")
-    public OpenAPI azureOpenAPI() {
+    public OpenAPI customOpenAPI() {
         Server azureServer = new Server();
         azureServer.setUrl("https://back-et.azurewebsites.net");
         azureServer.setDescription("Azure Production Server");
 
-        return new OpenAPI()
-                .info(new Info()
-                        .title("EasyTravel API")
-                        .description("API para sistema de viagens EasyTravel")
-                        .version("1.0.0"))
-                .servers(List.of(azureServer));
-    }
-
-    @Bean
-    @Profile("!prod")
-    public OpenAPI localOpenAPI() {
         Server localServer = new Server();
         localServer.setUrl("http://localhost:8080");
         localServer.setDescription("Local Development Server");
@@ -38,7 +27,13 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("EasyTravel API")
                         .description("API para sistema de viagens EasyTravel")
-                        .version("1.0.0"))
-                .servers(List.of(localServer));
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("DecolaTech Team")
+                                .email("contato@decolatech.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")))
+                .servers(List.of(azureServer, localServer));
     }
 }
